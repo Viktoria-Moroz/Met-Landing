4841111c1ee98af47fd0a3662ba07fbc88cfe73c
Viktoria Moroz <viktoriaandriivna25@gmail.com>
2024-10-01 12:31:21 +0300

    make build

---
+++ index.9867bf05.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("content--with-menu"):document.body.classList.remove("content--with-menu")}),document.querySelector(".form").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});
+//# sourceMappingURL=index.9867bf05.js.map
